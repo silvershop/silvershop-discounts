6e11e34836b4affb7ee11eee52975198d643c010
Nicolaas Francken <github@sunnysideup.co.nz>
2011-04-26 15:09:40 +0000

    a couple of small fixes to order coupon - they will probably need discussing.

--- code/model/OrderCoupon.php
+++ code/model/OrderCoupon.php
@@ -2,7 +2,7 @@
 
 /**
  *OrderCoupon
- * 
+ *
  *@author nicolaas [at] sunnysideup.co.nz, jeremy [at] burnbright.co.nz
  *
  **/
@@ -14,21 +14,27 @@ class OrderCoupon extends DataObject {
 		"Code" => "Varchar(25)",
 		"Value" => "Currency",
 		"PercentageDiscount" => "Decimal(4,2)",
-		
+
 		"StartDate" => "Date",
 		"EndDate" => "Date",
+		"DiscountAbsolute" => "Currency",
+		"DiscountPercentage" => "Decimal(4,2)",
+		//"CanOnlyBeUsedOnce" => "Boolean",
 
-		"MinOrderValue" => "Currency",		
+		//TODO: Order must be greater than...
+
+
+		"MinOrderValue" => "Currency",
 		"UseLimit" => 'Int'
 		//"Type" => "Enum('Voucher,GiftCard,Coupon','Coupon')" //for managing purposes
 		//"UseInConjunction" => "Boolean"
 	);
-	
+
 	public static $has_many = array(
 		//'FreeItems' => 'OrderItem'
 		//'SpecificProducts' => 'Product' //ie: not for any order
 	);
-	
+
 	public static $casting = array(
 		"UseCount" => "Int",
 		"IsValid" => "Boolean"
@@ -54,22 +60,24 @@ class OrderCoupon extends DataObject {
 	protected static $coupons_can_only_be_used_once = "";
 		static function set_coupons_can_only_be_used_once($b) {self::$coupons_can_only_be_used_once = $b;}
 		static function get_coupons_can_only_be_used_once() {return self::$coupons_can_only_be_used_once;}
-	
+
 	protected static $code_length = 10;
 		static function set_code_length($l) {self::$code_length = $l;}
 		static function get_code_length() {return self::$code_length;}
 
-	
+
 	public static $singular_name = "Order Coupon";
+		function i18n_singular_name() { return _t("OrderCoupon.ORDERCOUPON", "Order Coupon");}
 
 	public static $plural_name = "Order Coupon";
+		function i18n_plural_name() { return _t("OrderCoupon.ORDERCOUPONS", "Order Coupons");}
 
 	public static $default_sort = "EndDate DESC, StartDate DESC";
-	
+
 	static function get_by_code($code){
 		return DataObject::get_one('OrderCoupon',"\"Code\" = UPPER('$code')");
 	}
-	
+
 	function UseCount() {
 		$objects = DataObject::get("OrderCouponModifier", "\"OrderCouponID\" = ".$this->ID);
 		if($objects) {
@@ -79,24 +87,24 @@ class OrderCoupon extends DataObject {
 	}
 
 	function IsValid() {
-		
-		if($this->UseLimit > 0 && $this->UseCount() < $this->UseLimit) {			
+
+		if($this->UseLimit > 0 && $this->UseCount() < $this->UseLimit) {
 			//$this->getForm()->sessionMessage(_t("OrderCoupon.LIMITREACHED","Limit of $this->UseLimit has been reached"),'bad');
 			return false;
 		}
-		
+
 		//TODO:check order minimum
-		
+
 		$startDate = strtotime($this->StartDate);
 		$endDate = strtotime($this->EndDate);
 		$today = strtotime("today");
 		$yesterday = strtotime("yesterday");
-		
+
 		if($this->EndDate && $endDate < $yesterday){
 			//$this->getForm()->sessionMessage(_t("OrderCoupon.EXPIRED","This coupon has already expired"),'bad');
 			return false;
 		}
-		
+
 		if($this->StartDate && $startDate > $today){
 			//$this->getForm()->sessionMessage(_t("OrderCoupon.TOOEARLY","It is too early to use this coupon"),'bad');
 			return false;
@@ -104,10 +112,10 @@ class OrderCoupon extends DataObject {
 
 		return true;
 	}
-	
+
 	function getDiscountValue($origionalvalue){
 		if($this->DiscountAbsolute) {
-			return $origionalvalue - abs($this->Value);			
+			return $origionalvalue - abs($this->Value);
 		}
 		if($this->PercentageDiscount) {
 			return   $origionalvalue * ($this->PercentageDiscount / 100);
@@ -129,20 +137,19 @@ class OrderCoupon extends DataObject {
 
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
-		
+
 		if(!$this->Code){
 			$this->Code = self::generateNewCode();
 		}
-		//TODO: only allow entering percentage, OR ammount ..ie javascript split the two
 		return $fields;
 	}
-	
+
 	function setCode($code){
 		$code = eregi_replace("[^[:alnum:]]", " ", $code); //EXPLAIN: what does this do?
 		$code = trim(eregi_replace(" +", "", $code)); //gets rid of any white spaces
-		$this->setField("Code", strtoupper($code));		
+		$this->setField("Code", strtoupper($code));
 	}
-	
+
 	/**
 	 * Static function for generating new codes.
 	 * @return string - the new code, in uppercase, based on the first x characters of md5(time())
@@ -154,6 +161,6 @@ class OrderCoupon extends DataObject {
 		}while(DataObject::get('OrderCoupon',"\"Code\" = '$code'"));
 		return $code;
 	}
-	
+
 }
 
--- code/modifiers/OrderCouponModifier.php
+++ code/modifiers/OrderCouponModifier.php
@@ -33,8 +33,10 @@ class OrderCouponModifier extends OrderModifier {
 	}
 
 	public static $singular_name = "Order Coupon Reduction";
+		function i18n_singular_name() { return _t("OrderCouponModifier.ORDERCOUPONREDUCTION", "Order Coupon Reduction");}
 
 	public static $plural_name = "Order Coupon Reductions";
+		function i18n_plural_name() { return _t("OrderCouponModifier.ORDERCOUPONREDUCTIONS", "Order Coupon Reductions");}
 
 // ######################################## *** other (non) static variables (e.g. protected static $special_name_for_something, protected $order)
 
@@ -56,8 +58,8 @@ class OrderCouponModifier extends OrderModifier {
 	}
 
 	public static function init_for_order($className) {
-		
-		
+
+
 	}
 
 // ######################################## *** form functions (e. g. showform and getform)
@@ -67,15 +69,18 @@ class OrderCouponModifier extends OrderModifier {
 		return true;
 	}
 
+	function getModifierForm($controller) {
+		return self::get_form();
+	}
 	static function get_form($controller) {
 		//Requirements::themedCSS("OrderCouponModifier");
 		//Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
 		//Requirements::javascript(THIRDPARTY_DIR."/jquery-form/jquery.form.js");
 		//Requirements::javascript(ECOMMERCE_COUPON_DIR."/javascript/OrderCouponModifier.js");
-		
-		
+
+
 		return new CouponForm(null,"CouponForm");
-		
+
 		/*
 		$fields = new FieldSet();
 		$fields->push(new TextField('CouponCode',_t("OrderCouponModifier.COUPON", 'Coupon')));
@@ -135,7 +140,7 @@ class OrderCouponModifier extends OrderModifier {
 	/**
 	*@return boolean
 	**/
-	public function CanRemove() {
+	public function CanBeRemoved() {
 		return true;
 	}
 
@@ -216,12 +221,12 @@ class OrderCouponModifier extends OrderModifier {
 	*@return float
 	**/
 
-	protected function LiveAmount() {
+	protected function LiveCalculationValue() {
 		if(!self::$actual_deductions) {
 			self::$actual_deductions = 0;
 			$subTotal = $this->LiveSubTotalAmount();
 			if($coupon = $this->OrderCoupon()) {
-				self::$actual_deductions = $coupon->getDiscountValue($subTotal); 
+				self::$actual_deductions = $coupon->getDiscountValue($subTotal);
 			}
 			if($subTotal < self::$actual_deductions) {
 				self::$actual_deductions = $subTotal;
@@ -260,34 +265,34 @@ class OrderCouponModifier extends OrderModifier {
 
 //TODO: shift some of this to Modifer_Form
 class CouponForm extends OrderModifierForm{
-	
+
 	function __construct($controller = null, $name){
-				 
+
 		$fields = new FieldSet();
 		$fields->push(new HeaderField('CouponHeading',_t("CouponForm.COUPONHEADING", 'Coupon/Voucher Code'),3));
 		$fields->push(new TextField('Code',_t("CouponForm.COUPON", 'Enter your coupon code if you have one.')));
 		$actions = new FieldSet(new FormAction('apply', _t("CouponForm.APPLY", 'Apply')));
 		$validator = new CouponFormValidator(array('Code'));
-		
+
 		parent::__construct($controller, $name, $fields, $actions, $validator);
 	}
-	
+
 	function apply($data,$form){
-		
+
 		$coupon = OrderCoupon::get_by_code($data['Code']);
-			
+
 		//add a new discount modifier to the cart, linking to the entered coupon
-		
+
 		//check it hasn't already been added/ used up
 		//create new modifier
 		$modifier = new OrderCouponModifier();
 		$modifier->OrderCouponID = $coupon->ID;
 		$modifier->write();
-		
+
 		ShoppingCart::add_new_modifier($modifier);
-		
+
 		//TODO: introduce retry/lockout time per IP address
-		
+
 		$successmessage = sprintf(_t("OrderCouponModifier.APPLIED",'%s - has been applied'),$coupon->Title);
 
 		//Order::save_current_order();
@@ -300,21 +305,21 @@ class CouponForm extends OrderModifierForm{
 		}
 		return;
 	}
-	
+
 }
 
 
 class CouponFormValidator extends RequiredFields{
-	
+
 	function php($data) {
 		$valid = parent::php($data);
 		//check the coupon exists, and can be used
 		if(!OrderCoupon::get_by_code($data['Code'])){
-			$this->validationError('Code',_t("OrderCouponModifier.NOTFOUND","Sorry, that coupon could not be found"),"bad");		
+			$this->validationError('Code',_t("OrderCouponModifier.NOTFOUND","Sorry, that coupon could not be found"),"bad");
 		}
-		return $valid;	
+		return $valid;
 	}
-	
+
 }
 
 //DELETE THESE:
@@ -324,7 +329,7 @@ class OrderCouponModifier_Form extends OrderModifierForm {
 	public function applycoupon($data, $form) {
 		if(isset($data['CouponCode'])) {
 			$newOption = Convert::raw2sql($request['CouponCode']);
-			
+
 			$order = ShoppingCart::current_order();
 			$modifiers = $order->Modifiers();
 			if($modifiers) {
@@ -334,7 +339,7 @@ class OrderCouponModifier_Form extends OrderModifierForm {
 					}
 				}
 			}
-			
+
 		}
 
 	}
