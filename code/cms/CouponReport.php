59a2536f061faffc93b6e979f4a2aaf9af11c40d
Mark Guinn <mark@adaircreative.com>
2013-08-16 11:45:56 +0100

    Fixes to the coupon report

--- code/cms/CouponReport.php
+++ code/cms/CouponReport.php
@@ -30,33 +30,34 @@ class CouponReport extends ShopPeriodReport{
 	
 	function getReportField(){
 		$field = parent::getReportField();
-		$field->addSummary("Total",array(
-			"Entered"=>"sum",
-			"Uses"=>"sum",
-			"Savings"=> array("sum","Currency->Nice")
-		));
+//		$field->addSummary("Total",array(
+//			"Entered"=>"sum",
+//			"Uses"=>"sum",
+//			"Savings"=> array("sum","Currency->Nice")
+//		));
 		return $field;
 	}
 	
 	function query($params){
 		$query = parent::query($params);
-		$query->setSelect(array(
-			"$this->periodfield AS FilterPeriod",
-			"OrderCoupon.*",
-			"\"Title\" AS \"Name\"",
-			"COUNT(OrderCouponModifier.ID) AS Entered",
-			"SUM(if($this->periodfield IS NOT NULL, 1, 0)) AS Uses",
-			"SUM(if($this->periodfield IS NOT NULL,OrderModifier.Amount,0)) AS Savings"
-		));
-		$query->innerJoin("OrderCouponModifier", "OrderCoupon.ID = OrderCouponModifier.CouponID");
-		$query->innerJoin("OrderAttribute", "OrderCouponModifier.ID = OrderAttribute.ID");
-		$query->innerJoin("OrderModifier", "OrderCouponModifier.ID = OrderModifier.ID");
-		$query->innerJoin("Order", "OrderAttribute.OrderID = Order.ID");
+		$query->setSelect("OrderCoupon.*")
+			->selectField($this->periodfield, 'FilterPeriod')
+			->selectField('Title', 'Name')
+			->selectField('COUNT("OrderCouponModifier"."ID")', 'Entered')
+			->selectField('SUM(if(' . $this->periodfield . ' IS NOT NULL, 1, 0))', 'Uses')
+			->selectField('SUM(if(' . $this->periodfield . ' IS NOT NULL, "OrderModifier"."Amount", 0))', 'Savings');
+
+		$query->addInnerJoin("OrderCouponModifier", "OrderCoupon.ID = OrderCouponModifier.CouponID");
+		$query->addInnerJoin("OrderAttribute", "OrderCouponModifier.ID = OrderAttribute.ID");
+		$query->addInnerJoin("OrderModifier", "OrderCouponModifier.ID = OrderModifier.ID");
+		$query->addInnerJoin("Order", "OrderAttribute.OrderID = Order.ID");
+
 		$query->setGroupBy("OrderCoupon.ID");
 		if(!$query->getOrderBy()){
 			$query->setOrderBy("Savings DESC,Title ASC");
 		}
 		$query->setLimit("50");
+
 		return $query;
 	}
 	
