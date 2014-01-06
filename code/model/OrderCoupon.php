75e5ebe0f42601b60866a4de7cad3695dc593e5c
Mark Guinn <mark@adaircreative.com>
2014-01-06 11:53:28 +0000

    Fix for empty option on group dropdown

--- code/model/OrderCoupon.php
+++ code/model/OrderCoupon.php
@@ -130,6 +130,7 @@ class OrderCoupon extends DataObject {
 			
 			$maintab->Fields()->push($grps = new DropdownField("GroupID", "Member Belongs to Group", DataObject::get('Group')->map('ID','Title')));
 			$grps->setHasEmptyDefault(true);
+			$grps->setEmptyString('-- Any Group --');
 			
 			if($this->Type == "Percent"){
 				$fields->insertBefore($percent = new NumericField("Percent","Percentage discount"), "Active");
