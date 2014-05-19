silvershop/silvershop-discounts

commit 7143678d8520f33f3c7ea569f3ee643248ca6f01

Adding model validation support for minimum code length (off by default)

code/model/OrderCoupon.php
@@ -25,6 +25,7 @@ class OrderCoupon extends Discount {
 	private static $singular_name = "Coupon";
 	private static $plural_name = "Coupons";
 	public static $generated_code_length = 10;
+	private static $minimum_code_length = null;
 
 	public static function get_by_code($code) {
 		return self::get()
@@ -62,6 +63,24 @@ class OrderCoupon extends Discount {
 			"Active"
 		);
 		return $fields;
+	}
+
+	public function validate() {
+		$result = parent::validate();
+
+		$minLength = $this->config()->minimum_code_length;
+		if($minLength !== null && (strlen($this->getField('Code')) < $minLength)) {
+			$result->error(
+				_t(
+					'OrderCoupon.INVALIDMINLENGTH',
+					'Coupon code must be at least {length} characters in length',
+					array('length' => $this->config()->minimum_code_length)
+				),
+				'INVALIDMINLENGTH'
+			);
+		}
+
+		return $result;
 	}
 
 	/**

tests/OrderCouponTest.php
@@ -18,6 +18,9 @@ class OrderCouponTest extends SapphireTest{
 	public function setUp() {
 		parent::setUp();
 		ShopTest::setConfiguration();
+
+		Config::inst()->update('OrderCoupon', 'minimum_code_length', null);
+
 		$this->socks = $this->objFromFixture("Product", "socks");
 		$this->socks->publish("Stage", "Live");
 		$this->tshirt = $this->objFromFixture("Product", "tshirt");
@@ -28,6 +31,24 @@ class OrderCouponTest extends SapphireTest{
 		$this->placedorder = $this->objFromFixture("Order", "unpaid");
 		$this->cart = $this->objFromFixture("Order", "cart");
 		$this->othercart = $this->objFromFixture("Order", "othercart");
+	}
+
+	public function testMinimumLengthCode() {
+		Config::inst()->update('OrderCoupon', 'minimum_code_length', 8);
+
+		$coupon = new OrderCoupon(array('Code' => '1234567'));
+		$result = $coupon->validate();
+		$this->assertContains('INVALIDMINLENGTH', $result->codeList());
+
+		$coupon = new OrderCoupon(array('Code' => '12345678'));
+		$result = $coupon->validate();
+		$this->assertNotContains('INVALIDMINLENGTH', $result->codeList());
+
+		Config::inst()->update('OrderCoupon', 'minimum_code_length', null);
+
+		$coupon = new OrderCoupon(array('Code' => '1'));
+		$result = $coupon->validate();
+		$this->assertNotContains('INVALIDMINLENGTH', $result->codeList());
 	}
 
 	public function testPercent() {

commit 915ee9c8eabf8c116e90f94806c9799c4cd05fd5

Fixing regression in 7143678d85 where generating a code doesn't work

code/model/OrderCoupon.php
@@ -69,7 +69,9 @@ class OrderCoupon extends Discount {
 		$result = parent::validate();
 
 		$minLength = $this->config()->minimum_code_length;
-		if($minLength !== null && (strlen($this->getField('Code')) < $minLength)) {
+		$code = $this->getField('Code');
+
+		if($minLength && $code && strlen($code) < $minLength) {
 			$result->error(
 				_t(
 					'OrderCoupon.INVALIDMINLENGTH',

tests/OrderCouponTest.php
@@ -40,6 +40,10 @@ class OrderCouponTest extends SapphireTest{
 		$result = $coupon->validate();
 		$this->assertContains('INVALIDMINLENGTH', $result->codeList());
 
+		$coupon = new OrderCoupon();
+		$result = $coupon->validate();
+		$this->assertNotContains('INVALIDMINLENGTH', $result->codeList(), 'Leaving the Code field generates a code');
+
 		$coupon = new OrderCoupon(array('Code' => '12345678'));
 		$result = $coupon->validate();
 		$this->assertNotContains('INVALIDMINLENGTH', $result->codeList());