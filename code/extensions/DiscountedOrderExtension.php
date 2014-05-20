51e42f9df0757043138caca33a9c8eaa42e4c5fb
Jeremy Shipman <jeremy@burnbright.net>
2014-05-21 09:21:50 +1200

    FIX: typo in sql escaping for pgsql

--- code/extensions/DiscountedOrderExtension.php
+++ code/extensions/DiscountedOrderExtension.php
@@ -10,7 +10,7 @@ class DiscountedOrderExtension extends DataExtension{
 			->leftJoin("OrderDiscountModifier_Discounts", "\"Discount\".\"ID\" = \"OrderDiscountModifier_Discounts\".\"DiscountID\"")
 			->leftJoin("Product_OrderItem_Discounts", "\"Discount\".\"ID\" = \"Product_OrderItem_Discounts\".\"DiscountID\"")
 			->innerJoin("OrderAttribute", 
-				"(\"OrderDiscountModifier_Discounts.\"OrderDiscountModifierID\" = \"OrderAttribute\".\"ID\") OR 
+				"(\"OrderDiscountModifier_Discounts\".\"OrderDiscountModifierID\" = \"OrderAttribute\".\"ID\") OR 
 				(\"Product_OrderItem_Discounts\".\"Product_OrderItemID\" = \"OrderAttribute\".\"ID\")"
 			)
 			->filter("OrderAttribute.OrderID", $this->owner->ID);
