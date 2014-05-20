a5cf1692b3e228d5f7a32caacf97f58394c88464
Jeremy Shipman <jeremy@burnbright.net>
2014-05-20 17:11:37 +1200

    Postgres compatiblity fix

--- code/extensions/DiscountedOrderExtension.php
+++ code/extensions/DiscountedOrderExtension.php
@@ -7,11 +7,11 @@ class DiscountedOrderExtension extends DataExtension{
 	 */
 	function Discounts(){
 		return Discount::get()
-			->leftJoin("OrderDiscountModifier_Discounts", "Discount.ID = OrderDiscountModifier_Discounts.DiscountID")
-			->leftJoin("Product_OrderItem_Discounts", "Discount.ID = Product_OrderItem_Discounts.DiscountID")
+			->leftJoin("OrderDiscountModifier_Discounts", "\"Discount\".\"ID\" = \"OrderDiscountModifier_Discounts\".\"DiscountID\"")
+			->leftJoin("Product_OrderItem_Discounts", "\"Discount\".\"ID\" = \"Product_OrderItem_Discounts\".\"DiscountID\"")
 			->innerJoin("OrderAttribute", 
-				"(OrderDiscountModifier_Discounts.OrderDiscountModifierID = OrderAttribute.ID) OR 
-				(Product_OrderItem_Discounts.Product_OrderItemID = OrderAttribute.ID)"
+				"(\"OrderDiscountModifier_Discounts.\"OrderDiscountModifierID\" = \"OrderAttribute\".\"ID\") OR 
+				(\"Product_OrderItem_Discounts\".\"Product_OrderItemID\" = \"OrderAttribute\".\"ID\")"
 			)
 			->filter("OrderAttribute.OrderID", $this->owner->ID);
 	}
